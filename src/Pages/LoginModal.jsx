83c84fa84d205abf1bd22be7cfa8950d9913c4d0
Ashil Thomas <ashilthomas31@gmail.com>
2025-02-21 18:38:59 +0530

    button error

--- src/Pages/LoginModal.jsx
+++ src/Pages/LoginModal.jsx
@@ -4,11 +4,12 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { Button } from "@/components/ui/Button";
+
 import { useToast } from "@/hooks/use-toast";
 import instance from "@/Utils/Axios";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/Redux/userData";
+import { Button } from "@/Components/ui/button";
 
 // ✅ Validation schemas using Yup
 const loginSchema = yup.object().shape({
