3b9f55a56e4251a82a507e3922c9c99ed68a76b4
Ashil Thomas <142432081+ashilthomas@users.noreply.github.com>
2025-08-15 21:05:20 +0530

    dropDown optimized

--- src/Components/DropDownBtn/DropDownBtn.jsx
+++ src/Components/DropDownBtn/DropDownBtn.jsx
@@ -1,19 +1,21 @@
 "use client";
 
 import * as React from "react";
-
-
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import {   DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem, } from "../ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "../ui/dropdown-menu";
 
-export function DropDownBtn({logOut}) {
-    const {  role } = useSelector((state) => state.user);
-    console.log(role);
-    
+export const DropDownBtn = React.memo(function DropDownBtn({ logOut }) {
+  const { role } = useSelector((state) => state.user);
+
+  // Determine Post Job path based on role
+  const postJobPath =
+    role === "admin" || role === "employer" ? "/admin" : role === "job_seeker" ? "/employer" : null;
 
   return (
     <DropdownMenu>
@@ -22,47 +24,39 @@ export function DropDownBtn({logOut}) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth={1.5}
           stroke="currentColor"
-          class="size-6"
+          className="size-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
           />
         </svg>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent>
-        <Link to={"/myjob"}>
-        <DropdownMenuItem>Profile</DropdownMenuItem></Link>
-        {
-            role == "admin"||role === 'employer' &&   <Link to={"/admin"}>
-       
-          <DropdownMenuItem>Admin</DropdownMenuItem>
-     
-
+        <Link to="/myjob">
+          <DropdownMenuItem>Profile</DropdownMenuItem>
         </Link>
 
-        }
-      
-      
+        {(role === "admin" || role === "employer") && (
+          <Link to="/admin">
+            <DropdownMenuItem>Admin</DropdownMenuItem>
+          </Link>
+        )}
 
-        <span onClick={logOut}>
+        <DropdownMenuItem onClick={logOut}>Logout</DropdownMenuItem>
 
-   
-        <DropdownMenuItem>Logout</DropdownMenuItem>     </span>
-        {role === 'admin' || role === 'employer' ? (
-        <Link to="/admin">
-          <DropdownMenuItem>Post Job</DropdownMenuItem>
-        </Link>
-      ) : role === 'job_seeker' ? (
-        <Link to="/employer">
-          <DropdownMenuItem>Post Job</DropdownMenuItem>
-        </Link>
-      ) : null}
+        {postJobPath && (
+          <Link to={postJobPath}>
+            <DropdownMenuItem>Post Job</DropdownMenuItem>
+          </Link>
+        )}
+
         <DropdownMenuItem>Settings</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+});
